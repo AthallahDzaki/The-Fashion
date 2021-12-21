00531a9de861912922a2f5ee9b856b7f21922aef
AthallahDzaki <AthallahDzaki@gmail.com>
2021-12-21 16:50:32 +0700

    A

--- assets/js/item.js
+++ assets/js/item.js
@@ -1,3 +1,8 @@
+/*
+Author       : Athallah Dzaki.
+Template Name: The Fashion - eCommerce Bootstrap 5 HTML
+Version      : 1.0
+*/
 
 // Item
 
@@ -6,65 +11,175 @@
 // const itemRealPrice = ["$55.25", "$95.00", "$99.00", "$89.00", "$70.00", "$65.50", "$75.00"]
 // const itemDiscount = ["35% Off", "40% Off", "30% Off", "45% Off", "60% Off", "25% Off", "20% Off"]
 
+let phoneNumber = getCookie("phonenumber") || "6281332504194";
+
 const item = {
-    "ExclusiveProduct": [
-        { // 0 | 1
-            itemName: "Blue Dress Women",
-            itemPrice: "$45.00",
-            itemRealPrice: "$55.25",
-            itemDiscount: "35% Off"
-        }, { // 1 | 2
-            itemName: "Lether Gray Tuxedo",
-            itemPrice: "$55.00",
-            itemRealPrice: "$95.00",
-            itemDiscount: "40% Off"
-        }, { // 2 | 3
-            itemName: "Woman Full Sliv Dress",
-            itemPrice: "$59.40",
-            itemRealPrice: "$99.00",
-            itemDiscount: "30% Off"
-        }, { // 3 | 4
-            itemName: "Light Blue Shirt",
-            itemPrice: "$71.29",
-            itemRealPrice: "$89.00",
-            itemDiscount: "45% Off"
-        }, { // 4 | 5
-            itemName: "Blue Dress For Woman",
-            itemPrice: "$45.00",
-            itemRealPrice: "$70.00",
-            itemDiscount: "40% Off"
-        }
-    ]
+    "ExclusiveProduct": {
+        type: "discount",
+        data: [
+            { // 0 | 1
+                itemName: "Blue Dress Women",
+                itemPrice: "$45.00",
+                itemRealPrice: "$55.25",
+                itemDiscount: "35% Off",
+                itemImage: {
+                    light: "assets/img/dark/product_img1.jpg",
+                    dark: "assets/img/light/product_img1.jpg"
+                }
+            }, { // 1 | 2
+                itemName: "Lether Gray Tuxedo",
+                itemPrice: "$55.00",
+                itemRealPrice: "$95.00",
+                itemDiscount: "40% Off",
+                itemImage: {
+                    light: "assets/img/dark/product_img2.jpg",
+                    dark: "assets/img/light/product_img2.jpg"
+                }
+            }, { // 2 | 3
+                itemName: "Woman Full Sliv Dress",
+                itemPrice: "$59.40",
+                itemRealPrice: "$99.00",
+                itemDiscount: "30% Off",
+                itemImage: {
+                    light: "assets/img/dark/product_img3.jpg",
+                    dark: "assets/img/light/product_img3.jpg"
+                }
+            }, { // 3 | 4
+                itemName: "Light Blue Shirt",
+                itemPrice: "$71.29",
+                itemRealPrice: "$89.00",
+                itemDiscount: "45% Off",
+                itemImage: {
+                    light: "assets/img/dark/product_img4.jpg",
+                    dark: "assets/img/light/product_img4.jpg"
+                }
+            }, { // 4 | 5
+                itemName: "Blue Dress For Woman",
+                itemPrice: "$45.00",
+                itemRealPrice: "$70.00",
+                itemDiscount: "40% Off",
+                itemImage: {
+                    light: "assets/img/dark/product_img5.jpg",
+                    dark: "assets/img/light/product_img5.jpg"
+                }
+            }, { // 5 | 6
+                itemName: "Blue Casual Check Shirt",
+                itemPrice: "$45.00",
+                itemRealPrice: "$65.00",
+                itemDiscount: "40% Off",
+                itemImage: {
+                    light: "assets/img/dark/product_img6.jpg",
+                    dark: "assets/img/light/product_img6.jpg"
+                }
+            }, { // 6 | 7
+                itemName: "White Black Line Dress",
+                itemPrice: "$45.00",
+                itemRealPrice: "$75.00",
+                itemDiscount: "40% Off",
+                itemImage: {
+                    light: "assets/img/dark/product_img7.jpg",
+                    dark: "assets/img/light/product_img7.jpg"
+                }
+            }, { // 7 | 8
+                itemName: "Men Blue Jins Shirt",
+                itemPrice: "$36.00",
+                itemRealPrice: "$72.00",
+                itemDiscount: "50% Off",
+                itemImage: {
+                    light: "assets/img/dark/product_img8.jpg",
+                    dark: "assets/img/light/product_img8.jpg"
+                }
+            }
+        ]
+    },
+    "WomenProduct": {
+        type: "default",
+        data: [
+            { // 0 | 1
+                itemName: "Blue Dress Women",
+                itemPrice: "$45.00",
+                itemRealPrice: "$55.25",
+                itemDiscount: "35% Off",
+                itemDescription : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia, amet pariatur blanditiis quaerat reiciendis necessitatibus quidem eius eligendi, velit facere ratione veniam, quia saepe quo accusamus iste voluptatum tenetur obcaecati.",
+                itemImage: {
+                    light: "assets/img/dark/product_img1.jpg",
+                    dark: "assets/img/light/product_img1.jpg"
+                }
+            }, { // 1 | 2
+                itemName: "Woman Full Sliv Dress",
+                itemPrice: "$59.40",
+                itemRealPrice: "$99.00",
+                itemDiscount: "30% Off",
+                itemDescription: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia, amet pariatur blanditiis quaerat reiciendis necessitatibus quidem eius eligendi, velit facere ratione veniam, quia saepe quo accusamus iste voluptatum tenetur obcaecati.",
+                itemImage: {
+                    light: "assets/img/dark/product_img3.jpg",
+                    dark: "assets/img/light/product_img3.jpg"
+                }
+            }
+        ]
+    }
 }
 
-for(x in item) {
-    for(let i = 0; i < item[x].length; i++){
-        let itemName = item[x][i].itemName,
-            itemPrice = item[x][i].itemPrice,
-            itemRealPrice = item[x][i].itemRealPrice,
-            itemDiscount = item[x][i].itemDiscount,
-            itemIMG = item[x][i].itemImage
-            itemRating = item[x][i].itemRating;
-        
+for (x in item) {
+    const data = item[x].data
+    for (let i = 0; i < data.length; i++) {
+        let itemName = data[i].itemName,
+            itemPrice = data[i].itemPrice,
+            itemRealPrice = data[i].itemRealPrice,
+            itemDiscount = data[i].itemDiscount,
+            itemIMGLight = data[i].itemImage.light,
+            itemIMGDark = data[i].itemImage.dark,
+            itemDescription = data[i].itemDescription !== undefined ? data[i].itemDescription : "";
+
+        const discountCard = `
+        <div class="col">
+            <div class="card h-100 shadow discount">
+                <img id="Product" class="img-product" lightImg="${itemIMGLight}" darkImg="${itemIMGDark}"
+                    class="card-img-top Produk" alt="${itemName}" />
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">${itemName}</h5>
+                    <p>Price : <strong>${itemPrice}</strong> <a style="text-decoration: line-through;">${itemRealPrice}</a>
+                        <a style="color:green">${itemDiscount}</a>
+                    </p>
+                    <button class="btn btn-info d-block mt-auto" onclick="Buy('${itemName}', '${itemPrice}', '${itemRealPrice}', '${itemDiscount}')">Buy</button>
+                    <br />
+                    <a type="button" class="btn btn-info d-block mt-auto" data-toggle="tooltip" data-placement="bottom" title="Coming Soon">
+                        Info
+                    </a>
+                </div>
+            </div>
+        </div>
+        `
+
+        const defaultCard = `
+        <div class="col">
+            <div class="card h-100 shadow">
+                <img id="Product" class="img-product" lightImg="${itemIMGLight}" darkImg="${itemIMGDark}"
+                    class="card-img-top Produk" alt="${itemName}" />
+                <div class="card-body">
+                    <h5 class="card-title">${itemName}</h5>
+                    <p>Price : <strong>${itemPrice}</strong>
+                        ${itemDiscount !== "" ? `<a style="text-decoration: line-through;">${itemRealPrice}</a>  <a style="color:green">${itemDiscount}</a>` : ''}
+                    </p>
+                    <br />
+                    <p class="card-text">${itemDescription}.</p>
+                    <a class="btn btn-info d-block mt-auto" onclick="Buy('${itemName}', '${itemPrice}', '${itemRealPrice}', '${itemDiscount}')">Buy</a>
+                    <br />
+                    <a type="button" class="btn btn-info d-block mt-auto" data-toggle="tooltip" data-placement="bottom" title="Coming Soon">
+                        Info
+                    </a>
+                </div>
+            </div>
+        </div>
+        `
+
         let card = document.createElement("div");
         card.className = "col";
-        card.innerHTML = `
-        <div class="card h-100 shadow">
-            <img id="Product" src="https://bestwebcreator.com/shopwise/demo/assets/images/product_img1.jpg"
-                class="card-img-top Produk" alt="${itemName}" />
-            <div class="card-body d-flex flex-column">
-                <h5 class="card-title">${itemName}</h5>
-                <p>Price : <strong>${itemPrice}</strong> <a style="text-decoration: line-through;">${itemRealPrice}</a>
-                    <a style="color:green">${itemDiscount}</a>
-                </p>
-                <a class="btn btn-info d-block mt-auto" onclick="Buy('${itemName}', '${itemPrice}', '${itemRealPrice}', '${itemDiscount}')">Buy</a>
-            </div>
-        </div>`
+        card.innerHTML = item[x].type == "default" ? defaultCard : discountCard
         document.getElementById(x).appendChild(card);
     }
 }
 
-function Buy(name, price, realprice, discount)
-{
-    window.open(`https://wa.me/send?phone=6281332504194?&text=Product Name:%20${name}%20Real Price:%20${price}%0AReal Price:%20${realprice}%0ADiscount:%20${discount}%0A%0AThanks%20BRO!`)
+function Buy(name, price, realprice, discount) {
+    window.open(`https://wa.me/${phoneNumber}?&text=Product%20Name:%20${name}%20Real Price:%20${price}%0AReal Price:%20${realprice}%0ADiscount:%20${discount}%0A%0AThanks%20BRO!`);
 }
--- assets/js/system.js
+++ assets/js/system.js
@@ -1,50 +1,61 @@
-$(document).ready(function () {
+/*
+Author       : Athallah Dzaki.
+Template Name: The Fashion - eCommerce Bootstrap 5 HTML
+Version      : 1.0
+*/
+
+window.addEventListener('resize', function (event) {
+    console.log("P");
     if (document.documentElement.clientWidth < 575) {
         $("#navbarSection").removeAttr("class")
+    } else {
+        document.getElementById("navbarSection").classList.add("container-md");
     }
-})
-$(window).resize(function () {
-    // Do something more useful
-    if ($(window).height()) {
-        location.reload();
-    };
-});
+}, true);
 // device detection
 function detectMob() {
     return ((window.innerWidth <= 800) && (window.innerHeight <= 700));
 }
 if (detectMob()) {
-    $("#CarouselSell").attr("hidden", true);
-    $("#navbarSection").removeAttr("class")
+    document.getElementById("CarouselSell").setAttribute("hidden", true);
+    document.getElementById("navbarSection").removeAttribute("class")
 }
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
 var darkMode = document.getElementById('darkMode');
 
 var darkLabel = "darkModeLabel";
-var clicked = false;
 
 window.addEventListener('load', function () {
+    if (document.documentElement.clientWidth < 575) {
+        $("#navbarSection").removeAttr("class")
+    }
+    $("#onload-popup").modal("show")
     if (darkMode) {
         initTheme();
     }
 });
-
-$(document).ready(function() {
-    alert("Ini Website Belom Jadi\nProduct Masih Kurang");
-})
 
 document.getElementById("DarkModeButton").addEventListener("change", (event) => {
     resetTheme();
 });
 
 function ChangeImage(type) {
-    const x = document.getElementsByClassName("Produk");
-    if (type == 0) {
-        for (let i = 0; i < document.getElementsByClassName("Produk").length; i++) {
-            x[i].src = `assets/img/light/product_img${i + 1}.jpg`;
+    const x = document.getElementsByClassName("img-product")
+    if (type == 1) {
+        for (let i = 0; i < x.length; i++) {
+            const img = x[i].getAttribute("lightImg");
+            if (img === "") continue;
+            x[i].src = img;
         }
-    } else if (type == 1) {
-        for (let i = 0; i < document.getElementsByClassName("Produk").length; i++) {
-            x[i].src = `assets/img/dark/product_img${i + 1}.jpg`;
+    } else if (type == 0) {
+        for (let i = 0; i < x.length; i++) {
+            const img = x[i].getAttribute("darkImg");
+            if (img === "") continue;
+            x[i].src = img;
         }
     }
 
@@ -54,18 +65,180 @@ function initTheme() {
     var darkThemeSelected = localStorage.getItem('darkMode') !== null && localStorage.getItem('darkMode') === 'dark';
     darkThemeSelected ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
     darkThemeSelected ? ChangeImage(1) : ChangeImage(0);
+    darkThemeSelected ? document.getElementById("DarkModeButton").checked = false : document.getElementById("DarkModeButton").checked = true;
+
 }
 
 function resetTheme() {
-    if (clicked == false) {
+    let checked = document.getElementById("DarkModeButton").checked;
+    if (checked == false || checked == null) {
         document.body.setAttribute('data-theme', 'dark');
         localStorage.setItem('darkMode', 'dark');
+        document.getElementsByClassName("navbar-toggler-icon").style = "color: white;"
+
         ChangeImage(1);
-        clicked = true;
     } else {
         document.body.removeAttribute('data-theme');
         localStorage.removeItem('darkMode');
+        document.getElementsByClassName("navbar-toggler-icon").style = "color: black;"
         ChangeImage(0);
-        clicked = false;
     }
+}
+
+for (let i = 0; i < document.getElementsByClassName(".background_bg").length; i++) {
+    let attr = document.getElementsByClassName(".background_bg")[i].getAttribute("data-img-src");
+    if (typeof attr !== typeof undefined && attr !== false) {
+        document.getElementsByClassName(".background_bg").style = 'background-image', 'url(' + attr + ')';
+    }
+}
+
+$('.content-popup').magnificPopup({
+    type: 'inline',
+    preloader: true,
+    mainClass: 'mfp-zoom-in',
+});
+
+$('.image_gallery').each(function () { // the containers for all your galleries
+    $(this).magnificPopup({
+        delegate: 'a', // the selector for gallery item
+        type: 'image',
+        gallery: {
+            enabled: true,
+        },
+    });
+});
+
+$('.popup-ajax').magnificPopup({
+    type: 'ajax',
+    callbacks: {
+    }
+});
+
+$('.video_popup, .iframe_popup').magnificPopup({
+    type: 'iframe',
+    removalDelay: 160,
+    mainClass: 'mfp-zoom-in',
+    preloader: false,
+    fixedContentPos: false
+});
+
+$.magnificPopup.defaults.callbacks = {
+    open: function () {
+        $('body').addClass('zoom_image');
+    },
+    close: function () {
+        // Wait until overflow:hidden has been removed from the html tag
+        setTimeout(function () {
+            $('body').removeClass('zoom_image');
+            $('body').removeClass('zoom_gallery_image');
+            //$('.zoomContainer:last-child').remove();// remove zoom container from DOM
+            $('.zoomContainer').slice(1).remove();
+        }, 100);
+    }
+};
+
+// Set up gallery on click
+var galleryZoom = $('#pr_item_gallery');
+galleryZoom.magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+        enabled: true
+    },
+    callbacks: {
+        elementParse: function (item) {
+            item.src = item.el.attr('data-zoom-image');
+        }
+    }
+});
+
+var image = $('#product_img');
+//var zoomConfig = {};
+var zoomActive = false;
+
+zoomActive = !zoomActive;
+if (zoomActive) {
+    if ($(image).length > 0) {
+        $(image).elevateZoom({
+            cursor: "crosshair",
+            easing: true,
+            gallery: 'pr_item_gallery',
+            zoomType: "inner",
+            galleryActiveClass: "active"
+        });
+    }
+}
+else {
+    $.removeData(image, 'elevateZoom');//remove zoom instance from image
+    $('.zoomContainer:last-child').remove();// remove zoom container from DOM
+}
+
+$.magnificPopup.defaults.callbacks = {
+    open: function () {
+        $('body').addClass('zoom_image');
+    },
+    close: function () {
+        // Wait until overflow:hidden has been removed from the html tag
+        setTimeout(function () {
+            $('body').removeClass('zoom_image');
+            $('body').removeClass('zoom_gallery_image');
+            //$('.zoomContainer:last-child').remove();// remove zoom container from DOM
+            $('.zoomContainer').slice(1).remove();
+        }, 100);
+    }
+};
+
+// Set up gallery on click
+var galleryZoom = $('#pr_item_gallery');
+galleryZoom.magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+        enabled: true
+    },
+    callbacks: {
+        elementParse: function (item) {
+            item.src = item.el.attr('data-zoom-image');
+        }
+    }
+});
+
+// Zoom image when click on icon
+$('.product_img_zoom').on('click', function () {
+    var atual = $('#pr_item_gallery a').attr('data-zoom-image');
+    $('body').addClass('zoom_gallery_image');
+    $('#pr_item_gallery .item').each(function () {
+        if (atual == $(this).find('.product_gallery_item').attr('data-zoom-image')) {
+            return galleryZoom.magnificPopup('open', $(this).index());
+        }
+    });
+});
+
+function setPhone(number) {
+    phoneNumber = number;
+    setCookie("phonenumber", number, 365)
+    console.log("Number phone has been set to", number);
+    console.log("Note: This number is TEMP Number, if you want permanently change goto item.js instead");
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
